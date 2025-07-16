893853715dfd74768e79fe905a0a4a8ea387256a
Pedro Jaime <pejotadev07@outlook.com>
2025-07-16 12:22:15 -0300

    adicionando média para as novas notas

--- script.js
+++ script.js
@@ -4,17 +4,25 @@ const aluno = {
     calcularMedia(){
         let soma = 0
         this.notas.forEach(e => soma += e )
-        media = soma / this.notas.length
+        let media = soma / this.notas.length
         return media
     }
 }
 
 const {nome} = aluno
 const novasNotas = [...aluno.notas, 9] // eu usaria o push() para isso
+const mediaNovaNota = function media() {
+    let soma = 0
+    novasNotas.forEach(e => soma += e)
+    return media = soma / novasNotas.length
+}
 
 console.log(nome);
 console.log(aluno.calcularMedia());
 console.log(novasNotas);
+console.log(mediaNovaNota());
+
+
 
 
 
